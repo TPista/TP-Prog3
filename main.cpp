012086f2ef72dd2900e7c8a068d06928672ba812
carro123 <44232262+carro123@users.noreply.github.com>
2018-11-13 23:52:54 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,62 +1,208 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <stack>
+#include <cmath>
+
 using namespace std;
+/* Operadores matematicos */
+#define N_operators 6
+const string signos[N_operators] = {"+", "-", "*", "/", "%", "^"};
+int precedences[N_operators] = {1, 1, 2, 2, 2, 3};
+
+bool operador( const string );
+int precedencia( const string );
+
+/* Mis sugerencias personales a este programa:
+ *
+ * - Considerar el cambio del nombre standby por aux_stack, u otro.
+ * - Incorporar el analizador lexico para expresiones numericas de punto
+ *   flotante, o que ocupen mas de un caracter.
+ * - Eliminar la cadena intermedia postfix, pasando directamente los
+ *   resultados de la pila de operadores extraidos de infix, a una pila
+ *   de evaluacion.
+ * - Incorporar operadores unarios, como sin(), cos(), exp(), log(), etc.
+ * - Detectar errores de sintaxis en la expresion infija.
+ * - Otros que sean recomendados.
+ */
+
 int main() {
-    int numeros[] = {3,2,1,5,4};
-    int aux, pos, i, j;
 
-//primer for hago una lectura de la fila de enteros, donde tomo cada numero en el auxiliar
-    for  (i=0; i<5 ; i++){
-        pos = i;
-        aux = numeros [i];
+    string infijo, postfijo, token;
+    stack <string> standby;
+    stack <double> result;
+    size_t i;
+    char c;
+    double A, B;
 
-     //en el while comparo el valor del aux con la posicion del siguiente y lo corro hacia la izquierda si es mas chico
+    /* Cadena de entrada */
+    cout << "Intro expresion infija:";
+    getline( cin, infijo );
+    cout << endl;
 
-        while ((pos>0) && numeros [ pos-1 ] > aux){
+    /*
+      PRIMERA PARTE: Procesar la cadena infijo, y crear posfijo
+     */
+    for ( i = 0; i < infijo.size(); i++ ) {
+        /* esto debe cambiar luego a un token o palabra devuelta por
+         * el analizador léxico */
+        c = infijo[i];
+        token.clear();
+        token += c;			/* parece burdo, pero no conozco mejor manera de
+    								 * crear un string a partir de un unico caracter */
 
-            numeros[pos]= numeros [pos-1];
+        /* es un espacio: despreciar */
+        if ( c == ' ' ) continue;
 
-            pos --;
+        cout << "Analizando token: '" << c << "'" << endl;
+
+        /* es un carácter numérico: pasar al posfijo */
+        if ( c >= '0' && c <= '9' ) {
+            cout << "\tes numero: pasado a posfijo" << endl << endl;
+            postfijo = postfijo + " " + c;
+            continue;
         }
-        numeros [pos] = aux;
-    }
 
-    cout<<"Ascendente Inserción : "; //comienzo a recorrer de manera ascendente
-    for(i=0;i<5;i++){
-
-        cout<<numeros[i]<<" ";
-    }
-
-
-    cout<< "\nDescendente Inserción: "; //comienzo a recorrer de forma descendente
-    for(i=4; i>=0; i--){
-
-        cout<<numeros[i]<< " ";
-    }
-
-    //BURBUJEO
-
-    for(i=0; i<5; i++){                        //
-        for(j=0; j<5; j++){
-            if(numeros[j] > numeros[ j+1]){
-                numeros[ j]= numeros [ j+1];
-                numeros [ j+1]= aux;
+        /* si se lee un operador: sacar de la pila y pasar al postfijo
+         * todos los operadores con una precedencia mayor o igual a la
+         * suya, y depositar el mismo en la pila */
+        if ( operador( token ) ) {
+            cout << "\tes operador:" << endl;
+            while ( !standby.empty() && precedencia( standby.top() )
+                                        >= precedencia( token ) ) {
+                cout << "\tpasado operador '" + standby.top() +
+                        "' de la pila a posfijo" << endl;
+                postfijo = postfijo + " " + standby.top();
+                standby.pop();
             }
+            standby.push( token );
+            cout << "\tcolocar '" << token << "' en la pila" << endl << endl;
+            continue;
+        }
+
+        /* si se lee "(": colocar en la pila */
+        if ( token == "(") {
+            cout << "pasado a posfijo" << endl << endl;
+            standby.push( token );
+            continue;
+        }
+
+        /* si se lee ")": retirar de la pila hasta encontrar '(', y pasar
+         * los elementos retirados a posfijo, luego descartar el "(" */
+        if ( token == ")" ) {
+            while ( !standby.empty() && standby.top() != "(" ) {
+                cout << "\tpasado operador '" + standby.top() +
+                        "' de la pila a posfijo" << endl << endl;
+                postfijo = postfijo + " " + standby.top();
+                standby.pop();
+            }
+            if ( !standby.empty() )
+                standby.pop();	/* descartar el "(" */
         }
     }
 
-    cout<< "\nAscendente Burbujeo: ";
-    for(i=0;i<5;i++){
-        cout<<numeros[i]<<" ";
+    /* extraer de la pila cualquier operador restante y pasarlo a la cadena posfijo */
+    while ( !standby.empty() ) {
+        cout << "Pasado operador '" + standby.top() +
+                "' de la pila a posfijo" << endl << endl;
+        postfijo = postfijo + " " + standby.top();
+        standby.pop();
     }
 
+    /* Imprimir el posfijo */
+    cout << "Posfijo es: \n\t" << postfijo << endl << endl;
 
-    cout<<"\n Descendente Burbujeo: ";
+    /*
+      SEGUNDA PARTE: Procesar la cadena posfijo, y devolver resultado
+     */
 
-    for(i=4; i>=0; i--){
+    A = 0;
+    cout << "Evaluando la expresion ..." << endl;
+    for ( i = 0; i < postfijo.size(); i++ ) {
 
-        cout<<numeros[ i]<<" ";
+        c = postfijo[i];
+        token.clear();
+        token += c;
+
+        /* si se lee un operando (caracter numerico), depositar en la pila */
+        if ( c >= '0' && c <= '9' ) {
+            result.push( c - '0' );
+            continue;
+        }
+
+        /* si se lee un operador binario, poner en A y B los últimos dos argumentos
+         * de la pila y operarlos, guardando el resultado en la pila */
+        if ( operador( token ) ) {
+            if ( !result.empty() ) {
+                B = result.top();
+                result.pop();
+            }
+            else {
+                cout << "Argumentos insuficientes para '" << c << "'" << endl;
+                return -1;
+            }
+
+            if ( !result.empty() ) {
+                A = result.top();
+                result.pop();
+            }
+            else {
+                cout << "Argumentos insuficientes para '" << c << "'" << endl;
+                return -1;
+            }
+
+            cout << "\toperar " << A << token << B << " = ";
+            if ( token == "+" ) {
+                A += B;
+                result.push( A );
+            }
+            else if ( token == "-" ) {
+                A -= B;
+                result.push( A );
+            }
+            else if ( token == "*" ) {
+                A *= B;
+                result.push( A );
+            }
+            else if ( token == "/" ) {
+                A /= B;
+                result.push( A );
+            }
+            else if ( token == "%" ) {
+                A = (int )A % (int )B;
+                result.push( A );
+            }
+            else if ( token == "^" ) {
+                A = pow(A, B);
+                result.push( A );
+            }
+            cout << A << endl;
+        }
     }
 
+    if ( !result.empty() )
+        cout << endl << "El resultado es: " << result.top() << endl;
 
+    return 0;
+};
 
-    }
+/* Verdadero si el token corresponde a un operador. */
+bool operador( const string token ) {
+
+    for ( int i = 0; i < N_operators; i++ )
+        if ( signos[i] == token )
+            return true;
+
+    return false;
+}
+
+/* Devuelve la precedencia del operador descrito por el
+ * string token (-1 si no es un operador) */
+int precedencia( const string token ) {
+
+    for ( int i = 0; i < N_operators; i++ )
+        if ( signos[i] == token )
+            return precedences[i];
+
+    return -1;
+}
